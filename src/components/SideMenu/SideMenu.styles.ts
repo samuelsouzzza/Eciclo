b720da368f915f2fc70df8fbada2155783ae4416
Samuel R Souza <rssamuel17@gmail.com>
2023-08-30 16:41:08 -0300

    Ícones do menu principal adicionados

--- src/components/SideMenu/SideMenu.styles.ts
+++ src/components/SideMenu/SideMenu.styles.ts
@@ -7,4 +7,27 @@ export const Container = styled.aside`
   height: 100%;
   width: 100%;
   grid-area: sidenav;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding: 5%;
+  .i {
+    font-size: 1.25rem;
+    cursor: pointer;
+    padding: 10% 2%;
+    width: 90%;
+    border-radius: 5px;
+    transition: 0.3s;
+    &:hover {
+      background-color: #92e3a944;
+    }
+    &.new {
+      background-color: #92e3a944;
+      border: 1px solid #92e3a9;
+      &:hover {
+        background-color: #92e3a9;
+      }
+    }
+  }
 `;
--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import { Container } from './SideMenu.styles.ts';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUser,
+  faGear,
+  faPlus,
+  faQuestion,
+  faComment,
+  faListUl,
+  faBell,
+} from '@fortawesome/free-solid-svg-icons';
 
 export const SideMenu = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <FontAwesomeIcon icon={faUser} className='i' />
+      <FontAwesomeIcon icon={faPlus} className='i new' />
+      <FontAwesomeIcon icon={faBell} className='i' />
+      <FontAwesomeIcon icon={faListUl} className='i' />
+      <FontAwesomeIcon icon={faComment} className='i' />
+      <FontAwesomeIcon icon={faQuestion} className='i' />
+      <FontAwesomeIcon icon={faGear} className='i' />
+    </Container>
+  );
 };
