fee0a40a11699703e535086d983b980836f2bc23
Samuel R Souza <rssamuel17@gmail.com>
2023-09-04 22:23:39 -0300

    Importações sem uso apagadas no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './pages/Login/Login';
 import { NewAccount } from './pages/NewAccount/NewAccount';
 import { Home } from './pages/Home/Home';
 import { Page404 } from './pages/Page404/Page404';
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='new_account' element={<NewAccount />} />
-          <Route path='home' element={<Home />} />
-          <Route path='*' element={<Page404 />} />
-        </Routes>
-      </QueryClientProvider>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='new_account' element={<NewAccount />} />
+        <Route path='home' element={<Home />} />
+        <Route path='*' element={<Page404 />} />
+      </Routes>
     </BrowserRouter>
   );
 }
