32eb5d4dbe0fa9678599ece98253e796e3023041
Samuel R Souza <rssamuel17@gmail.com>
2023-09-21 18:26:30 -0300

    Verificação de sessão do usuário adicionada

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,9 +4,16 @@ import { Feed } from '../../components/Feed/Feed';
 import { SideMenu } from '../../components/SideMenu/SideMenu.tsx';
 import { UseContextScreens } from '../../global/ScreenStates.tsx';
 import { MenuNewPublication } from '../../components/MenuNewPublication/MenuNewPublication.tsx';
+import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
   const { showFeed, showModalNewPublication } = UseContextScreens();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const userLogged = localStorage.getItem('userLogged');
+    if (!userLogged) navigate('/');
+  }, []);
 
   return (
     <Container>
